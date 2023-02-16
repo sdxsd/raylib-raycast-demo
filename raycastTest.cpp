0f049d5f5683262804d060a4599e682774fde4e3
sdxsd <willmaguire@icloud.com>
2023-02-16 17:19:00 +0100

    Getting closer to rendering.

--- raycastTest.cpp
+++ raycastTest.cpp
@@ -7,16 +7,12 @@ int main(void) {
 	Image			frame;
 
 	RayCaster	rayCaster;
-	Vector2 cameraPosition = {10, 14};
-	Vector2 dirVector = {-1, 0};
-	Vector2 planeVector = {0, 0.66};
-
 	InitWindow(WIN_WIDTH, WIN_HEIGHT, "Raycast-raylib-demo");
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
-		// frame = rayCaster.renderFrame();
-		// frameTex = LoadTextureFromImage(frame);
+		frame = rayCaster.renderFrame();
+		frameTex = LoadTextureFromImage(frame);
 		BeginDrawing();
 			DrawTexture(frameTex, 0, 0, WHITE);
 		EndDrawing();
