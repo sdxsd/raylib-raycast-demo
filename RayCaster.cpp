ac876329c272ab5036da63f4fe291bb3194c0794
sdxsd <willmaguire@icloud.com>
2023-02-21 15:53:38 +0100

    Refactoring.

--- RayCaster.cpp
+++ RayCaster.cpp
@@ -27,55 +27,54 @@ static Color getWallColor(char element) {
 	return (color);
 }
 
-VertLine RayCaster::castRay(RayCamera &rayCam, int x) {
-	VertLine result;
-	RayVariables ray;
-	ray.cameraX = 2 * x / double(WIN_WIDTH) - 1;
-	ray.rayDir = {
-		rayCam.dV.x + rayCam.pV.x * ray.cameraX,
-		rayCam.dV.y + rayCam.pV.y * ray.cameraX
-	};
-	ray.ImapCoords = { int(rayCam.camPos.x), int(rayCam.camPos.y) };
-	ray.deltaDistances = { abs(1 / ray.rayDir.x), abs(1 / ray.rayDir.y) };
-	double perpWallDist;
-	int stepX;
-	int stepY;
-	int hit = 0;
-	int side;
-
+static void	setupStepDirection(RayVariables& ray, RayCamera& rayCam) {
 	if (ray.rayDir.x < 0) {
-		stepX = -1;
+		ray.stepDirection.x = -1;
 		ray.sideDistances.x = (rayCam.camPos.x - ray.ImapCoords.x) * ray.deltaDistances.x;
 	} else {
-		stepX = 1;
+		ray.stepDirection.x = 1;
 		ray.sideDistances.x = (ray.ImapCoords.x + 1.0 - rayCam.camPos.x) * ray.deltaDistances.x;
 	}
 	if (ray.rayDir.y < 0) {
-		stepY = -1;
+		ray.stepDirection.y = -1;
 		ray.sideDistances.y = (rayCam.camPos.y - ray.ImapCoords.y) * ray.deltaDistances.y;
 	} else {
-		stepY = 1;
+		ray.stepDirection.y = 1;
 		ray.sideDistances.y = (ray.ImapCoords.y + 1.0 - rayCam.camPos.y) * ray.deltaDistances.y;
 	}
+}
+
+static int castUntilHit(RayVariables& ray, Map& map) {
+	int	hit = 0;
+	int	side;
+
 	while (hit == 0) {
 		if (ray.sideDistances.x < ray.sideDistances.y) {
 			ray.sideDistances.x += ray.deltaDistances.x;
-			ray.ImapCoords.x += stepX;
+			ray.ImapCoords.x += ray.stepDirection.x;
 			side = 0;
 		} else {
 			ray.sideDistances.y += ray.deltaDistances.y;
-			ray.ImapCoords.y += stepY;
+			ray.ImapCoords.y += ray.stepDirection.y;
 			side = 1;
 		}
 		if (map.getCoord(ray.ImapCoords.x, ray.ImapCoords.y) == '#') {
 			hit = 1;
 		}
 	}
+	return (side);
+}
+
+VertLine genLine(RayVariables& ray, Map& map, int x) {
+	VertLine	result;
+	double		perpWallDist;
+	Color			color;
+	int				side;
+
 	if (side == 0)
 		perpWallDist = (ray.sideDistances.x - ray.deltaDistances.x);
 	else
 		perpWallDist = (ray.sideDistances.y - ray.deltaDistances.y);
-
 	int lineHeight = (int)((WIN_HEIGHT / perpWallDist));
 	int drawStart = -lineHeight / 2 + WIN_HEIGHT / 2;
 	if (drawStart < 0)
@@ -83,9 +82,7 @@ VertLine RayCaster::castRay(RayCamera &rayCam, int x) {
 	int drawEnd = lineHeight / 2 + WIN_HEIGHT / 2;
 	if (drawEnd >= WIN_HEIGHT)
 		drawEnd = WIN_HEIGHT - 1;
-	Color color = getWallColor(map.getCoord(ray.ImapCoords.x, ray.ImapCoords.y));
-
-	// give x and y sides different brightness
+	color = getWallColor(map.getCoord(ray.ImapCoords.x, ray.ImapCoords.y));
 	if (side == 1) {
 		color.r -= 30, color.b -= 10, color.g -= 10;
 	};
@@ -94,6 +91,21 @@ VertLine RayCaster::castRay(RayCamera &rayCam, int x) {
 	result.endPoint = drawEnd;
 	result.color = color;
 	return (result);
+}
+
+VertLine RayCaster::castRay(RayCamera &rayCam, int x) {
+	VertLine			result;
+	RayVariables	ray;
+	int						side;
+
+	ray.cameraX = 2 * x / double(WIN_WIDTH) - 1;
+	ray.rayDir.x = rayCam.dV.x + rayCam.pV.x * ray.cameraX;
+	ray.rayDir.y = rayCam.dV.y + rayCam.pV.y * ray.cameraX;
+	ray.ImapCoords = { int(rayCam.camPos.x), int(rayCam.camPos.y) };
+	ray.deltaDistances = { abs(1 / ray.rayDir.x), abs(1 / ray.rayDir.y) };
+	setupStepDirection(ray, rayCam);
+	side = castUntilHit(ray, map);
+	return (genLine(ray, map, x));
 }
 
 void	RayCaster::drawVert(VertLine line) {
